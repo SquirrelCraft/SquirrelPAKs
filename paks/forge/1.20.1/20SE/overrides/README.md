8a9d65671800ddb4b2401a4cac037b28aa0706e4
The Network Squirrel <network_squirrel@squirrelcraft.org>
2024-12-01 14:36:15 -0500

    SquirrelPAK 20SE - Special Edition v1.0.0-1.20.1
    SquirrelPAK 20SE Release 1.0.0
    File Version 1.0.0-1.20.1
    
    Required Forge Version: 47.3.7
    
    First Release
    
    Summary:
    It's here! Fist release of 20SE. If you find any issues
    with the PAK raise and issue on our GitHub Page

---
+++ paks/forge/1.20.1/20SE/overrides/README.md
@@ -0,0 +1,32 @@
+# SquirrelPAK 20SE - Special Edition
+
+> Last call for 1.20.1! - Last PAK for 1.20.1, you don't have to play it but you really should!
+
+In our last PAK (really this time) of the 1.20.1 series we reviewed all the modes in 20KS (Kitchen Sink) and then we hand picked the very best mods. Next we cloned 20VP (Vanilla Plus) removed some mods, added all the best mods, and on stream tested and tweaked this perfect ending to 1.20.1 PAK series. This PAK is special and thats why we called it 20SE - Special Edition
+  
+This PAK is special because it has all good stuff without the fluff. We looked at our first PAK MC12, and we looked to see why we loved that PAK so much. That guided us to pick just the right mods from 1.20.1 Kitchen Sink PAK. We tested the alpha builds on stream over at our YouTube channel, and the result is this very Special Edition of SquirrelPAK for 1.20.1 
+
+We will also be streaming a Lets Play on our YouTube channel using this PAK to finish out our Lets Play 1.20.1 series.  
+
+For the full mod list in the pack check out the Mod Pack Deps page for up to date list.
+
+This is truly the last PAK in the 1.20.1 seres, we promise! Why? Because we are working on 1.21 and have no more time for 1.20.1 development.
+
+
+```
+  
+Issues/Questions/Documentation/Source
+
+Questions/Issues: Submit and issue on our GitHub tracker
+https://github.com/SquirrelCraft/SquirrelPAKs/labels/20SE
+ 
+Source: Source code can be found on GitHub
+https://github.com/SquirrelCraft/SquirrelPAKs/tree/main/paks/forge/1.20.1/20SE
+ 
+ 
+-=-=-=-=-=-=-=-= Social Media =-=-=-=-=-=-=-=-=-
+Connect with us on Social Media!
+
+https://squirrelcraft.live
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+```
---
+++ paks/forge/1.20.1/20SE/overrides/squirrelpak/bin/make_new_ver.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+#
+#     Internal script
+#     Creates new version
+#     make_new_ver.sh
+#     v3.0
+# 
+
+#     make_new_ver.sh - Creates a new version
+#     Copyright (C) 2024 The Network Squirrel(SquirrelCraft)
+#     https://github.com/SquirrelCraft/squirrelpak-scripts     
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ 
+echo " "
+echo " ----------------------------------------------------------------------------"
+echo "  SquirrelPAK Make New Version Script v3.0"
+echo "  (ver.sh) - Licnesed under GNU GPLv3"
+echo " ----------------------------------------------------------------------------"
+echo " | Copyright (C) 2024 The Network Squirrel(SquirrelCraft)                   |"
+echo " | https://github.com/SquirrelCraft/squirrelpak-scripts                     |"
+echo " | This program comes with ABSOLUTELY NO WARRANTY; This is free software,   |"
+echo " | and you are welcome to redistribute it under certain conditions          |"
+echo " ----------------------------------------------------------------------------"
+echo " "
+
+Script_Dir=$PWD 
+PAK_ChangeLog=./changelog.txt
+PAK_Removed_Files_Base_Dir=./zz-do-not-export
+PAK_Export_Timestamp_File=$PAK_Removed_Files_Base_Dir/exported.timestamp
+PAK_Tools_Save_Dir=$Script_Dir/squirrelpak/pak-tools
+
+
+# We should be in the root dir where the changelog is
+# located, if not exit
+
+if [ "$PAK_Debug " == "true " ]; then echo "Script: Checking running directory..."; fi
+
+if [ ! -f $PAK_ChangeLog ]; then
+    echo " "
+    echo "PWD=$PWD"
+    echo "Script must run in the root folder!"
+    echo "Script exiting!"
+    echo " "
+    exit 1
+fi
+
+
+# Debug output
+if [ "$PAK_Debug " == "true " ]; then
+    echo "PWD=$PWD"
+    echo "Script: Checking running directory (Ok)"
+    echo " "
+fi
+
+
+# Set local vars
+PAK_bin_dir=./squirrelpak/bin
+PAK_etc_dir=./squirrelpak/etc
+PAK_versons_dir=./squirrelpak/versions
+
+
+# Debug output
+
+if [ "$PAK_Debug " == "true " ]; then
+    echo "Local Vars Set!"
+    echo "PAK_bin_dir=$PAK_bin_dir"
+    echo "PAK_etc_dir=$PAK_etc_dir"
+    echo "PAK_versons_dir=$PAK_versons_dir"
+    echo "PAK_Export_Timestamp_File=$PAK_Export_Timestamp_File"
+	echo "PAK_Removed_Files_Base_Dir=$PAK_Removed_Files_Base_Dir"
+	echo "PAK_Tools_Save_Dir=$PAK_Tools_Save_Dir"
+    echo " "
+fi
+
+
+# Check for undo file
+if [ ! -f "$PAK_etc_dir/version.txt.undo" ]; then
+	echo "  Error: Undo File not found "
+	echo "  Exiting script, nothing done"
+	echo " ----------------------------------------------------------------------------"
+	exit 1
+fi
+
+
+# Check for undo file
+if [ ! -f "$PAK_Export_Timestamp_File" ]; then
+	echo "  Error: Export time stamp file not found "
+	echo "  $PAK_Export_Timestamp_File"
+	echo "  Exiting script, nothing done"
+	echo " ----------------------------------------------------------------------------"
+	exit 1
+fi
+
+echo " "
+echo " Checks complete, ready to create new version!"
+printf "%s " "Press enter to continue"
+read ans
+echo " "
+
+echo " - Remove undo backup file"
+if [ -f $PAK_etc_dir/version.txt.undo.backup ]; then
+	echo "   - Found backup file, removing "
+	rm -v $PAK_etc_dir/version.txt.undo.backup 
+fi
+echo     "done!"
+echo " "
+echo " - Make backup of current undo file"
+mv -v $PAK_etc_dir/version.txt.undo $PAK_etc_dir/version.txt.undo.backup 
+echo "   done!"
+echo " "
+echo " - Create version undo file: $PAK_etc_dir/version.txt.undo"
+cp -v $PAK_etc_dir/version.txt $PAK_etc_dir/version.txt.undo
+echo "   done!"
+echo " "
+echo " - Create new version"
+echo "   - Update version.txt numbers via nano/vim"
+echo " "
+printf "%s " "Press enter to continue"
+read ans
+nano $PAK_etc_dir/version.txt
+echo "     done"
+echo " "
+
+echo "   - Update bcc-common.toml version via nano/vim"
+echo " "
+printf "%s " "Press enter to continue"
+read ans
+nano ./config/bcc-common.toml
+echo "     done"
+echo " "
+
+
+# Move Pak Tools back
+echo " "
+echo "   - Move Pak Tools back to mods dir"
+mv -v ./zz-do-not-export/squirrelpak-tools/* ./mods
+echo " "
+echo " Script Complete"
+echo " "
+echo " ----------------------------------------------------------------------------"
+
+
+
+if [ -f $PAK_Export_Timestamp_File.backup ]; then
+	echo " Found $PAK_Export_Timestamp_File.backup"
+	echo " Remove it!"
+	rm -v $PAK_Export_Timestamp_File.backup	
+fi
+echo " - Backup time stamp file"
+mv -v $PAK_Export_Timestamp_File $PAK_Export_Timestamp_File.backup
+echo " done!"
+echo " "
+echo " Script Complete"
+echo " "
+echo " ----------------------------------------------------------------------------"
